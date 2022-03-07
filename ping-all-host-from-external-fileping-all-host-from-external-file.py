62b3f0fc66efbabf41901258111c23c45ecb5e30
crisbia3 <91191901+crisbia3@users.noreply.github.com>
2022-03-07 13:56:52 +0100

    Create ping-all-host-from-external-fileping-all-host-from-external-file.py

---
+++ ping-all-host-from-external-fileping-all-host-from-external-file.py
@@ -0,0 +1,17 @@
+import subprocess
+
+hostsfile = open("hostlistping.csv", "r")
+
+lines = hostsfile.readlines()
+
+for line in lines:
+    response = subprocess.Popen(["ping", "-n", "1", line.strip()],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+
+    stdout, stderr = response.communicate()
+
+    if (response.returncode == 0):
+        status = line.rstrip() + " is Reachable"
+    else:
+        status = line.rstrip() + " is Not reachable"
+    print(status)
+    
